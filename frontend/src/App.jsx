1945a8fe340256bb1dcb9b4fa311452aeaa0b9e8
ShivamSharma19Ai <shivam.sharma19sept@gmail.com>
2024-09-05 18:38:24 +0530

    navbar created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Navbar from './components/Navbar'
+import Calendar from './pages/Calendar'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='bg-red-600'>App</h1>
+      < Navbar/>
+      < Calendar />
+      <h1 className="">App</h1>
+
     </>
   )
 }
