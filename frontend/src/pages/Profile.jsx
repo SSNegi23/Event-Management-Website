8477b44205327557819e48785737c54cfea7ad8d
ShivamSharma19Ai <shivam.sharma19sept@gmail.com>
2024-09-09 18:49:33 +0530

    Profile page completed

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -15,22 +15,56 @@ const Profile = () => {
               <div className="user-address"> CURRENT ADDRESS</div>
            </div>
            <div className="profile-top-right-container">
+              <div className="profile-top-right-container-elements-main">First Name</div>
+              <div className="profile-top-right-container-elements-main">Second Name</div>
+              <div className="profile-top-right-container-elements-secondary">John</div>
+              <div className="profile-top-right-container-elements-secondary">Doe</div>
+              <div className="profile-top-right-container-elements-main">Phone</div>
+              <div className="profile-top-right-container-elements-main">Email</div>
+              <div className="profile-top-right-container-elements-secondary">9811111111</div>
+              <div className="profile-top-right-container-elements-secondary">abc@gmail.com</div>
+           </div>
+
+            {/* 
             <form onSubmit={handleSubmit(onSubmit)}>
               <input type="text" placeholder="First name" {...register("First name", {required: true, maxLength: 80})} />
               <input type="text" placeholder="Last name" {...register("Last name", {required: true, maxLength: 100})} />
               <input type="text" placeholder="Email" {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} />
               <input type="tel" placeholder="Mobile number" {...register("Mobile number", {required: true, minLength: 6, maxLength: 12})} />
               <input type="submit" />
-              
             </form>
-           </div>
+            */}
+           
         </div>
         <div className="profile-bottom-container">
-        <div className="profile-bottom-left-container">c</div>
-        <div className="profile-bottom-right-container">d</div>
+        <div className="profile-bottom-left-container">
+          <div className="title">Media Handles</div>
+          <div className="tiles">
+            <p>Facebook: </p>
+            <p>john</p>
+          </div>
+          <div className="tiles">
+            <p>Instagram: </p>
+            <p>john</p>
+          </div>
+          <div className="tiles">
+            <p>Twitter: </p>
+            <p>john</p>
+          </div>
+        </div>
+        <div className="profile-bottom-right-container">
+          <div className="title">
+            Interests
+          </div>
+          <div>
+            <p className="interests">Cricket</p>
+            <p className="interests">Football</p>
+            <p className="interests">Fitness</p>
+            <p className="interests">Gaming</p>
+          </div>
+        </div>
         </div>
       </div>
-      <h1>Profile</h1>
     </div>
   )
 }
