8d2940edd81fae646935833dd242de8446df6f00
ShivamSharma19Ai <shivam.sharma19sept@gmail.com>
2024-10-19 19:00:28 +0530

    Profile menu update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,21 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate} from "react-router-dom";
+import { useState } from "react";
 import "../styles/Navbar.css";
 
 const Navbar = () => {
+ 
+  const [logoutMenu, SetlogoutMenu] = useState(false);
   const navigate = useNavigate();
+  const handleClick = (e) => {
+    SetlogoutMenu(!logoutMenu);
+    e.preventDefault();
+  }
+  const handleLogout = () =>{
+    console.log("hello")
+    localStorage.clear();
+    navigate('/signup');
+  }
+  
   return (
     <div className="h-38 bg-navbar-color container">
       <div className="w-24 h-24">
@@ -28,12 +41,20 @@ const Navbar = () => {
           </li>
           <li>
             <a href="">
-              <i className="fa-solid fa-user fa-2x"></i>
+              <i className="fa-solid fa-user fa-2x" onClick={(e) => {handleClick(e)}}>
+                <div className={ logoutMenu ? "block logoutmenudivstlye":"hidden"}>
+                <ul className="logoutmenustyle">
+                  <li className="logoutmenuli" onClick={() => { navigate('/profile');}}>Profile</li>
+                  <li className="logoutmenuli" onClick={handleLogout}>Logout</li>
+                </ul>
+                </div>
+              </i>
             </a>
           </li>
         </ul>
       </div>
     </div>
+    
   );
 };
 
