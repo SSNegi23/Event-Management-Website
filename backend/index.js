5616eda58423c3c8a4f2b5c15a9144dd80085ed6
ShivamSharma19Ai <shivam.sharma19sept@gmail.com>
2024-10-21 18:13:41 +0530

    Sign up and login functionality enabled

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,47 @@ app.use(express.json());
 app.use(cors());
 app.use("/uploads", express.static(path.join(__dirname, "uploads"))); // Serve static files
 
+// User signup route
+app.post("/signup", async (req, res) => {
+  let user = new User(req.body);
+  try {
+    let result = await user.save();
+    result = result.toObject();
+    delete result.password;
+    Jwt.sign({ result }, jwtKey, { expiresIn: "2h" }, (err, token) => {
+      if (err) {
+        return res.send({ result: "Something went wrong, please try again." });
+      }
+      res.send({ result, auth: token });
+    });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// User login route
+app.post("/login", async (req, res) => {
+  if (req.body.email && req.body.password) {
+    try {
+      let user = await User.findOne(req.body).select("-password");
+      if (user) {
+        Jwt.sign({ user }, jwtKey, { expiresIn: "2h" }, (err, token) => {
+          if (err) {
+            return res.send({ result: "Something went wrong, please try again." });
+          }
+          res.send({ user, auth: token });
+        });
+      } else {
+        res.send({ result: "No User Found" });
+      }
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  } else {
+    res.send({ result: "No User Found" });
+  }
+});
+
 // Configure multer storage
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
