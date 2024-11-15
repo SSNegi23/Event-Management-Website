72d9b853e4cba29093ac0d5b8f1020e54476d2a5
Shivang <negi1322003@gmail.com>
2024-11-15 18:01:38 +0530

    Routing Problem solved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,58 +1,64 @@
-import { Route, Routes , Navigate} from 'react-router-dom'
-import { useState , useEffect} from 'react'
-import './App.css'
-import MainContainer from './components/MainContainer'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import Profile from './pages/Profile'
-import Event from './pages/Event'
-import SearchEvents from './pages/SearchEvents'
-import MyCalendar from './pages/Calendar'
-import EventMaker from './pages/EventMaker'
-import Login from './pages/Login'
-import SignUp from './pages/SignUp'
+import { Route, Routes, Navigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import "./App.css";
+import MainContainer from "./components/MainContainer";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Event from "./pages/Event";
+import SearchEvents from "./pages/SearchEvents";
+import MyCalendar from "./pages/Calendar";
+import EventMaker from "./pages/EventMaker";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 
 function App() {
-  console.log("App.jsx");
-  const [isAuthentcated, setIsAuthenticated] = useState(false);
-  
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(()=> {
-    if(localStorage.getItem('token')!=null){
-      setIsAuthenticated(true);
-    };
-  }, [])
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setIsAuthenticated(!!token); // Set to true if token exists
+    setIsLoading(false); // Authentication check complete
+  }, []);
+
+  if (isLoading) {
+    // Display a loading spinner or message while authentication is checked
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
       <Navbar />
       <Routes>
-      { isAuthentcated ? (
-        <>
-      <Route path='home'  element={<MainContainer />}>
-        <Route path='home' element={<Home />} />
-        <Route path='calendar' element={<MyCalendar />} />
-        <Route path='search-event' element={<SearchEvents />} />
-      </Route>
-      <Route path='/' element={<SignUp />} />
-      <Route path='profile' element={<Profile />} />
-      <Route path='event' element={<Event />} />
-      <Route path='eventmaker' element={<EventMaker />}/>
-      <Route path='login' element={<Login />}/>
-      <Route path='signup' element={<SignUp />}/>
-      </>
-      ): (
-        <>
-        <Route path='login' element={<Login />}/>
-        <Route path='signup' element={<SignUp />}/>
-        <Route path="*" element={<Navigate to="/login" />} />
-        </>
-      )}
-      
-      
-        
+        {isAuthenticated ? (
+          <>
+            {/* Nested routes under MainContainer */}
+            <Route path="/" element={<MainContainer />}>
+              <Route index element={<Home />} />
+              <Route path="home" element={<Home />} />
+              <Route path="calendar" element={<MyCalendar />} />
+              <Route path="search-event" element={<SearchEvents />} />
+            </Route>
+            {/* Individual routes */}
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/event" element={<Event />} />
+            <Route path="/eventmaker" element={<EventMaker />} />
+            <Route path="/login" element={<Navigate to="/home" />} />
+            <Route path="/signup" element={<Navigate to="/home" />} />
+            {/* Fallback wildcard route */}
+            <Route path="*" element={<Navigate to="/home" />} />
+          </>
+        ) : (
+          <>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="*" element={<Navigate to="/login" />} />
+          </>
+        )}
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,28 +1,30 @@
 import { useEffect, useState } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import "../styles/Profile.css";
 
 const Profile = () => {
   const [details, setDetails] = useState([]);
   const [address, setaddress] = useState("Add Address");
-  const [changingaddress, setchangingaddress]= useState(false);
-  const [changemediahandles, setchangemediahandles]= useState(false);
-  const [Facebooklink, setFacebooklink]= useState("Link your Facebook");
-  const [Instagramlink, setInstagramlink]= useState("Link your Instagram");
-  const [Twitterlink,setTwitterlink]= useState("Link your Twitter");
-  const [Hobbies, setHobbies]= useState(["cricket","hockey"]);
+  const [changingaddress, setchangingaddress] = useState(false);
+  const [changemediahandles, setchangemediahandles] = useState(false);
+  const [Facebooklink, setFacebooklink] = useState("Link your Facebook");
+  const [Instagramlink, setInstagramlink] = useState("Link your Instagram");
+  const [Twitterlink, setTwitterlink] = useState("Link your Twitter");
+  const [Hobbies, setHobbies] = useState(["cricket", "hockey"]);
   const [newHobby, setnewHobby] = useState("");
 
-  useEffect(() =>  {
+  useEffect(() => {
     async function getUser() {
-      let user = localStorage.getItem('user');
+      let user = localStorage.getItem("user");
       user = JSON.parse(user);
       // console.log("user: ", user);
       // console.log("user id: ", user._id);
       try {
-        const response = await fetch(`http://localhost:5000/signup/${user._id}`, {
-          method: "GET",
-        })
+        const response = await fetch(
+          `http://localhost:5000/signup/${user._id}`,
+          {
+            method: "GET",
+          }
+        );
         const userDetails = await response.json();
         setDetails(userDetails);
         console.log(userDetails);
@@ -35,65 +37,102 @@ const Profile = () => {
   }, []);
 
   const changeaddress = () => {
-      setchangingaddress(!changingaddress);
-  }
+    setchangingaddress(!changingaddress);
+  };
 
   const saveaddress = (e) => {
     setaddress(e.target.value);
-  }
+  };
 
-  const changingmediahandles = () =>{
+  const changingmediahandles = () => {
     setchangemediahandles(!changemediahandles);
-  }
+  };
 
   const changefacebook = (e) => {
     setFacebooklink(e.target.value);
-  }
+  };
 
-  const changeinstagram = (e) =>{
+  const changeinstagram = (e) => {
     setInstagramlink(e.target.value);
-  }
-   
+  };
+
   const changetwitter = (e) => {
     setTwitterlink(e.target.value);
-  }
+  };
 
   const changehobby = (e) => {
     setnewHobby(e.target.value);
-  }
+  };
   const addhobby = (e) => {
     setHobbies([...Hobbies, newHobby]);
     setnewHobby("");
-  }
+  };
 
-  const deleteHobby = (hobbyToDelete) =>{
-    setHobbies(Hobbies.filter((hobby => hobby !== hobbyToDelete)));
+  const deleteHobby = (hobbyToDelete) => {
+    setHobbies(Hobbies.filter((hobby) => hobby !== hobbyToDelete));
     console.log(Hobbies);
-  }
+  };
 
   return (
     <div>
       <div className="profile-container">
         <div className="profile-top-container">
-           <div className="profile-top-left-container">
-              <div className="profile-img w-32 h-32"><img  src="src\assets\images\logo.jpg" className="rounded-full"></img></div>
-              <div className="user-address"> 
-                 { changingaddress ? <><input type="text" onChange={saveaddress}></input><i class="fa-solid fa-floppy-disk" onClick={changeaddress}></i></>: <><div className="user-address-heading">{address}</div><i class="fa-solid fa-pen-to-square" onClick={changeaddress}></i></> }
-              </div>
-           </div>
-           <div className="profile-top-right-container">
-              <div className="profile-top-right-container-elements-main">User Name</div>
-              <div className="profile-top-right-container-elements-main">Gender</div>
-              <div className="profile-top-right-container-elements-secondary">{details.name}</div>
-              <div className="profile-top-right-container-elements-secondary">{details.gender}</div>
-              <div className="profile-top-right-container-elements-main">Phone</div>
-              <div className="profile-top-right-container-elements-main">Email</div>
-              <div className="profile-top-right-container-elements-secondary">{details.phoneNumber}</div>
-              <div className="profile-top-right-container-elements-secondary">{details.email}</div>
-           </div>
-           {/* user {user._id} */}
+          <div className="profile-top-left-container">
+            <div className="profile-img w-32 h-32">
+              <img
+                src="src\assets\images\logo.jpg"
+                className="rounded-full"
+              ></img>
+            </div>
+            <div className="user-address">
+              {changingaddress ? (
+                <>
+                  <input type="text" onChange={saveaddress}></input>
+                  <i
+                    class="fa-solid fa-floppy-disk"
+                    onClick={changeaddress}
+                  ></i>
+                </>
+              ) : (
+                <>
+                  <div className="user-address-heading">{address}</div>
+                  <i
+                    class="fa-solid fa-pen-to-square"
+                    onClick={changeaddress}
+                  ></i>
+                </>
+              )}
+            </div>
+          </div>
+          <div className="profile-top-right-container">
+            <div className="profile-top-right-container-elements-main">
+              User Name
+            </div>
+            <div className="profile-top-right-container-elements-main">
+              Gender
+            </div>
+            <div className="profile-top-right-container-elements-secondary">
+              {details.name}
+            </div>
+            <div className="profile-top-right-container-elements-secondary">
+              {details.gender}
+            </div>
+            <div className="profile-top-right-container-elements-main">
+              Phone
+            </div>
+            <div className="profile-top-right-container-elements-main">
+              Email
+            </div>
+            <div className="profile-top-right-container-elements-secondary">
+              {details.phoneNumber}
+            </div>
+            <div className="profile-top-right-container-elements-secondary">
+              {details.email}
+            </div>
+          </div>
+          {/* user {user._id} */}
 
-            {/* 
+          {/* 
             <form onSubmit={handleSubmit(onSubmit)}>
               <input type="text" placeholder="First name" {...register("First name", {required: true, maxLength: 80})} />
               <input type="text" placeholder="Last name" {...register("Last name", {required: true, maxLength: 100})} />
@@ -102,73 +141,81 @@ const Profile = () => {
               <input type="submit" />
             </form>
             */}
-           
         </div>
         <div className="profile-bottom-container">
-        <div className="profile-bottom-left-container">
-          <div className="title">Media Handles </div>
-          { changemediahandles ? 
+          <div className="profile-bottom-left-container">
+            <div className="title">Media Handles </div>
+            {changemediahandles ? (
               <>
-              <div className="tiles">
-               <p>Facebook: </p>
-               <input type="text" onChange={changefacebook}></input>
-              </div>
-              <div className="tiles">
-               <p>Instagram: </p>
-               <input type="text" onChange={changeinstagram}></input>
-              </div>
-              <div className="tiles">
-               <p>Twitter: </p>
-               <input type="text" onChange={changetwitter}></input>
-              </div>
-              <i class="fa-solid fa-floppy-disk" onClick={changingmediahandles}></i>
+                <div className="tiles">
+                  <p>Facebook: </p>
+                  <input type="text" onChange={changefacebook}></input>
+                </div>
+                <div className="tiles">
+                  <p>Instagram: </p>
+                  <input type="text" onChange={changeinstagram}></input>
+                </div>
+                <div className="tiles">
+                  <p>Twitter: </p>
+                  <input type="text" onChange={changetwitter}></input>
+                </div>
+                <i
+                  class="fa-solid fa-floppy-disk"
+                  onClick={changingmediahandles}
+                ></i>
               </>
-              :
+            ) : (
               <>
-              <div className="tiles">
-               <p>Facebook: </p>
-               <p>{Facebooklink}</p>
-              </div>
-              <div className="tiles">
-               <p>Instagram: </p>
-               <p>{Instagramlink}</p>
-              </div>
-              <div className="tiles">
-               <p>Twitter: </p>
-               <p>{Twitterlink}</p>
-              </div>
-              <i class="fa-solid fa-pen-to-square" onClick={changingmediahandles}></i>
+                <div className="tiles">
+                  <p>Facebook: </p>
+                  <p>{Facebooklink}</p>
+                </div>
+                <div className="tiles">
+                  <p>Instagram: </p>
+                  <p>{Instagramlink}</p>
+                </div>
+                <div className="tiles">
+                  <p>Twitter: </p>
+                  <p>{Twitterlink}</p>
+                </div>
+                <i
+                  class="fa-solid fa-pen-to-square"
+                  onClick={changingmediahandles}
+                ></i>
               </>
-          }
-        </div>
-        <div className="profile-bottom-right-container">
-          <div className="title">
-            Interests
+            )}
           </div>
-          <div>
-            <input type="text" onChange={changehobby} value={newHobby}></input>
-            <i class="fa-solid fa-plus" onClick={addhobby}></i>
-            {
-              Hobbies.map((hobby, ind) => {
-                return(
-                  < div key={ind}>
-                  <p className="interests" >{hobby}</p>
-                  <i class="fa-solid fa-trash" onClick={() => deleteHobby(hobby)}></i>
+          <div className="profile-bottom-right-container">
+            <div className="title">Interests</div>
+            <div>
+              <input
+                type="text"
+                onChange={changehobby}
+                value={newHobby}
+              ></input>
+              <i class="fa-solid fa-plus" onClick={addhobby}></i>
+              {Hobbies.map((hobby, ind) => {
+                return (
+                  <div key={ind}>
+                    <p className="interests">{hobby}</p>
+                    <i
+                      class="fa-solid fa-trash"
+                      onClick={() => deleteHobby(hobby)}
+                    ></i>
                   </div>
-                )
-              })
-            }
-            {/* (ind) => {delete Hobbies[ind]}
+                );
+              })}
+              {/* (ind) => {delete Hobbies[ind]}
              <p className="interests">Cricket</p>
             <p className="interests">Football</p>
             <p className="interests">Fitness</p>
             <p className="interests">Gaming</p> */}
+            </div>
           </div>
-        </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
